daf8f70b9d7f13210ec6b72c14411f98691aa71a
Aluno <Aluno@200.17.90.201>
2017-08-18 10:49:08 -0300

    Fetch("User"): new list user.

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -9,7 +9,8 @@ INSERT into user(id, username, password, name, email, experience, skill, active,
 --ROLES
 insert into role(id, role) values
 (1, 'ROLE_USER'),
-(2, 'ROLE_ADMIN');
+(2, 'ROLE_ADMIN'),
+(3, 'ROLE_MANAGER');
 
 -- USER_ROLES
 insert into user_roles (user_id, roles_id) values
