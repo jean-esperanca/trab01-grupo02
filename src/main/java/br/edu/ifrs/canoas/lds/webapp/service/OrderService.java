961bed230dfe27311854dc72926ef9e45e1c5c4b
Lauro Pereira <lauro.s.pereira@gmail.com>
2017-08-18 10:18:15 -0300

    fetch(order): Create MVC and html for order

--- src/main/java/br/edu/ifrs/canoas/lds/webapp/service/OrderService.java
+++ src/main/java/br/edu/ifrs/canoas/lds/webapp/service/OrderService.java
@@ -1,8 +1,21 @@
 package br.edu.ifrs.canoas.lds.webapp.service;
 
+import br.edu.ifrs.canoas.lds.webapp.config.Messages;
+import br.edu.ifrs.canoas.lds.webapp.domain.Client;
+import br.edu.ifrs.canoas.lds.webapp.domain.Order;
+import br.edu.ifrs.canoas.lds.webapp.repository.ClientRepository;
 import org.springframework.stereotype.Service;
 
 import br.edu.ifrs.canoas.lds.webapp.repository.OrderRepository;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+import java.util.Locale;
 
 /*
  *  Create by Edward Ramos Aug/11/2017
@@ -15,6 +28,26 @@ public class OrderService {
 
 	public OrderService(OrderRepository orderRepository) {
 		this.orderRepository = orderRepository;
+	}
+
+	public Order save(Order order){
+		return orderRepository.save(order);
+	}
+
+	public Order getOne(Order order){
+		return orderRepository.findById(order.getId()).get();
+	}
+
+	public void delete(Long id){
+		orderRepository.deleteById(id);
+	}
+
+	public Order getId(Long id){
+		return orderRepository.getOne(id);
+	}
+
+	public Iterable<Order> list(){
+		return orderRepository.findAll();
 	}
 	
 }
