21dd74666e5fc8d254d0ba1cf9da512734efdf4d
Elvis <elvispiedade@yahoo.com.br>
2017-08-22 10:26:43 -0300

    Fetch(Product): Created product form and added link to product/form

--- src/main/java/br/edu/ifrs/canoas/lds/webapp/controller/ProductController.java
+++ src/main/java/br/edu/ifrs/canoas/lds/webapp/controller/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.edu.ifrs.canoas.lds.webapp.config.Messages;
+import br.edu.ifrs.canoas.lds.webapp.domain.Product;
 import br.edu.ifrs.canoas.lds.webapp.service.ProductService;
 
 /*
@@ -27,6 +28,14 @@ public class ProductController {
 	@GetMapping("list")
 	public ModelAndView list(){
 		return new ModelAndView("/product/list");
+	}
+	
+	@GetMapping("/create")
+	public ModelAndView create(){
+		ModelAndView mav = new ModelAndView("/product/form");
+		mav.addObject("product", new Product());
+		mav.addObject("isCreate", true);
+		return mav;
 	}
 	
 }
